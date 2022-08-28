3d41a38cf36e690277135894bac355fe3c0cf9b9
Javier Miron <javiermiron89@gmail.com>
2022-08-27 18:31:01 -0600

    Agregando Clase 5

---
+++ Clase5/src/Datos.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author javie
+ */
+public class Datos {
+    
+    public String nombre;
+    public String apellido;
+    public int monto;
+    
+    public Datos(String nombre, String apellido, int monto){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.monto = monto;
+    }
+    
+}
